a974ea4994818620e4cbf79e3de5ef43daaa443a
Rerique <efpri.nanda@gmail.com>
2022-08-12 18:46:36 +0700

    create header section

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Header from './components/header-section/Header';
 import NotesInput from './components/input-section/NotesInput';
 
 // import style
 import './styles/style.css';
 
 const root = createRoot(document.getElementById('root'));
-root.render(<NotesInput />);
+root.render([<Header />, <NotesInput />]);
