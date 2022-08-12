9e251c286af5009c09e2ddd298148782fde6567f
Rerique <efpri.nanda@gmail.com>
2022-08-12 18:28:26 +0700

    create input section

---
+++ src/components/input-section/NotesInput.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class NotesInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="note-input">
+        <h2>Buat Catatan</h2>
+        <form>
+          <p className="note-input__title__char-limit">Sisa Karakter: 50</p>
+          <input type="text" className="note-input__title" placeholder="JUDUL" />
+          <textarea type="text" className="note-input__body" placeholder="Catatan.."></textarea>
+          <button type="submit">Buat</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NotesInput;
