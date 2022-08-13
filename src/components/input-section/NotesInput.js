f9938b651c2042397395f8706ee03e81c90d3b5c
Rerique <efpri.nanda@gmail.com>
2022-08-13 19:25:09 +0700

    add limit char on title input

--- src/components/input-section/NotesInput.js
+++ src/components/input-section/NotesInput.js
@@ -16,7 +16,7 @@ class NotesInput extends React.Component {
 
   onTitleChangeEventHandler(event) {
     this.setState(() => {
-      return { title: event.target.value };
+      return { title: event.target.value.slice(0, 50) };
     });
   }
 
@@ -36,7 +36,7 @@ class NotesInput extends React.Component {
       <div className="note-input">
         <h2>Buat Catatan</h2>
         <form onSubmit={this.onSubmitEventHandler}>
-          <p className="note-input__title__char-limit">Sisa Karakter: 50</p>
+          <p className="note-input__title__char-limit">Sisa Karakter: {50 - this.state.title.length}</p>
           <input type="text" className="note-input__title" placeholder="Judul Catatan ..." onChange={this.onTitleChangeEventHandler} value={this.state.title} />
           <textarea type="text" className="note-input__body" placeholder="Isi catatan ..." onChange={this.onNoteChangeEventHandler} value={this.state.body}></textarea>
           <button type="submit">Buat</button>
